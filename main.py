412974283f992bdd569aac060712c3b13d426892
Nika <nika.ganjelashvili@makingscience.com>
2025-05-05 13:02:29 +0400

    feat: add main

---
+++ main.py
@@ -0,0 +1,13 @@
+from extract import extract
+from utils.settings import SETTINGS
+from utils.logger import logger
+
+def main():
+    try:
+        extract(SETTINGS.API_URL, SETTINGS.PARAMS)
+        logger.info("Data extraction completed successfully")
+    except Exception as e:
+        logger.error(f"Error occurred during data extraction: {e}")
+
+if __name__ == "__main__":
+    main()
