c5c7fbcb66661dcba58e7dfdc8a82c223fc0773f
Nika <nika.ganjelashvili@makingscience.com>
2025-05-06 15:26:32 +0400

    feat: pass collection name to upload_docs func

--- firestore.py
+++ firestore.py
@@ -1,14 +1,11 @@
 from google.cloud import firestore
-from  utils.settings import SETTINGS
+from utils.settings import SETTINGS
 
-db = firestore.Client()
 
 class Firestore:
     def __init__(self):
         self.db = firestore.Client(database=SETTINGS.FIRESTORE_DB)
-        self.collection = self.db.collection(SETTINGS.FIRESTORE_COLLECTION)
 
-    def upload_docs(self, docs):
+    def upload_docs(self, docs, date):
         for doc in docs:
-            self.collection.add(doc)
-
+            self.db.collection(date).add(doc)
