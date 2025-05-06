218e4c20bfed28cfe169a0b2fd9b912434092abd
Nika <nika.ganjelashvili@makingscience.com>
2025-05-06 15:23:04 +0400

    feat: pass missing_dates to main func

--- main.py
+++ main.py
@@ -4,15 +4,20 @@ from utils.logger import logger
 from firestore import Firestore
 
 def main(request):
-    if request:
-        request = request.get_json()
-        logger.info(f"Received request: {request}")
 
-    data = extract(SETTINGS.API_URL, SETTINGS.PARAMS)
-    Firestore().upload_docs(data["articles"])
+    missing_dates = request.get_json()["missing_dates"]
+    logger.info(f"Received request: {request}")
+
+    for date in missing_dates:
+        docs = extract(SETTINGS.API_URL, {
+            "from": date,
+            "to": date,
+            "apiKey": SETTINGS.API_KEY
+        })
+        Firestore().upload_docs(docs["articles"], date)
+
     logger.info("Data extraction completed successfully")
     return {
         "message": "Data extraction completed successfully",
         "status": "success"
     }
-
